f0670635d21d36ffda8f2b1f0e6cc934fbf9d3bb
imranmuha <imramuha@student.arteveldehogeschool.be>
2017-10-05 09:42:36 +0200

    Oefening 1-3
    commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+        <?php $naam = "Imran Muhammad"?>
+        <h1><?php echo $naam ?></h1>
+
+        <?php $string1 = "Hello "?>
+        <?php $string2 = "World"?>
+        <h3><?php echo $string1, $string2?></h3>
+
+        <?php $x = 10?>
+        <?php $y = 7?>
+
+        <h5><?php echo $x + $y?></h5>
+        <h5><?php echo $x - $y?></h5>
+        <h5><?php echo $x * $y?></h5>
+        <h5><?php echo $x % $y?></h5>
+    
+</body>
+</html>
