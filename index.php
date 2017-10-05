299ac800e7719a787c5cd5528fb33b64965e5378
imranmuha <imramuha@student.arteveldehogeschool.be>
2017-10-05 09:59:37 +0200

    Update

--- index.php
+++ index.php
@@ -9,17 +9,7 @@
         <?php $naam = "Imran Muhammad"?>
         <h1><?php echo $naam ?></h1>
 
-        <?php $string1 = "Hello "?>
-        <?php $string2 = "World"?>
-        <h3><?php echo $string1, $string2?></h3>
 
-        <?php $x = 10?>
-        <?php $y = 7?>
-
-        <h5><?php echo $x + $y?></h5>
-        <h5><?php echo $x - $y?></h5>
-        <h5><?php echo $x * $y?></h5>
-        <h5><?php echo $x % $y?></h5>
     
 </body>
 </html>
