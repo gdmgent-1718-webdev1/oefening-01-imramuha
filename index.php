79e087e5e34255531f70a30df583862c34f28dda
Imran <imramuha@student.arteveldehs.be>
2017-10-05 10:13:10 +0200

    commit

--- index.php
+++ index.php
@@ -6,12 +6,7 @@
 </head>
 <body>
 
-        <?php $naam = "Imran Muhammad"?>
-        <h1><?php echo $naam ?></h1>
-
-        <?php $string1 = "Hello "?>
-        <?php $string2 = "World"?>
-        <h3><?php echo $string1, $string2?></h3>
+     
 
         <?php $x = 10?>
         <?php $y = 7?>
